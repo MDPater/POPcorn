5cb6aff8c3847342778fd17597b23d00e7f992b4
MDPater <depatermax@gmail.com>
2023-08-22 19:25:05 +0200

    k

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:popcorn/data/movie.dart';
-import 'package:popcorn/widget/movie_item.dart';
-import 'package:http/http.dart' as http;
-=======
 import 'package:popcorn/widgets/toprated.dart';
 import 'package:popcorn/widgets/trending.dart';
 import 'package:tmdb_api/tmdb_api.dart';
->>>>>>> a26461953653678439c55f66e3a217a74bda1c68
+import 'package:popcorn/constants/api_constants.dart';
 
 String appbarTitle = "POPcorn";
 
@@ -21,9 +16,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List topratedMovies = [];
   List trendingMovies = [];
-  final String apikey = "8d28c2f1418b07cac8dfcdbfac0d3a44";
-  final String readaccesstoken =
-      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDI4YzJmMTQxOGIwN2NhYzhkZmNkYmZhYzBkM2E0NCIsInN1YiI6IjYzZGFjNjA1YTZjMTA0MDA4NTg3Y2YzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fVZtxE0HhKVu6Q9mIztpQoOtwosTr2qKp1eKAGrz8u8";
 
   @override
   //initializing on Start
@@ -35,7 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   //load Top rated Movies from TMDB
   loadmovies() async {
-    TMDB tmdbLogs = TMDB(ApiKeys(apikey, readaccesstoken),
+    TMDB tmdbLogs = TMDB(ApiKeys(API_Key, readaccesstoken),
         logConfig: const ConfigLogger(showLogs: true, showErrorLogs: true));
 
     Map topratedresult = await tmdbLogs.v3.movies.getTopRated();
