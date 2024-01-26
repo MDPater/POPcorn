bec989521137fd25c6fea30b18a155944e78cbce
Max de Pater <61990627+MDPater@users.noreply.github.com>
2024-01-26 12:13:42 +0100

    Update api_constants.dart

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,3 +1,2 @@
-const API_Key = '8d28c2f1418b07cac8dfcdbfac0d3a44';
-const readaccesstoken =
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDI4YzJmMTQxOGIwN2NhYzhkZmNkYmZhYzBkM2E0NCIsInN1YiI6IjYzZGFjNjA1YTZjMTA0MDA4NTg3Y2YzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fVZtxE0HhKVu6Q9mIztpQoOtwosTr2qKp1eKAGrz8u8';
+const API_Key = '';
+const readaccesstoken ='';
