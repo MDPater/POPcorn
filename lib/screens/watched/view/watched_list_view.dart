e2bd55c33a5d832c8177e9bbf4553fd0eb3f865b
MDPater <depatermax@gmail.com>
2024-11-29 00:43:36 +0100

    QoL

--- lib/screens/watched/view/watched_list_view.dart
+++ lib/screens/watched/view/watched_list_view.dart
@@ -53,173 +53,179 @@ class _Watched_ScreenState extends State<WatchedScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.secondary,
-      drawer: const MyNavDrawer(),
-      endDrawer: const MyProfileDrawer(),
-      appBar: const MyAppBar(),
-      body: Column(children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 50, width: 50),
-            const SizedBox(height: 50, width: 50),
-            Container(
-              padding:
-                  const EdgeInsets.only(top: 5, bottom: 5, left: 15, right: 15),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12), color: Colors.grey),
-              child: const Text(
-                "Your Watched Movies",
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            SizedBox(
-              height: 50,
-              width: 40,
-              child: IconButton(
-                icon: const Icon(
-                  Icons.search,
-                  color: Colors.deepPurple,
-                  size: 25,
-                ),
-                onPressed: () {
-                  //Search Movie from list
-                },
-              ),
-            ),
-            SizedBox(
-              height: 50,
-              width: 40,
-              child: IconButton(
-                icon: const Icon(
-                  Icons.view_list,
-                  color: Colors.deepPurple,
-                  size: 25,
-                ),
-                onPressed: () {
-                  //Filter list by different criteria
-                },
-              ),
-            ),
-          ],
-        ),
-        const Padding(padding: EdgeInsets.only(top: 10)),
-        SizedBox(
-          height: MediaQuery.of(context).size.height - 150,
-          child: ListView(
-            scrollDirection: Axis.vertical,
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        drawer: const MyNavDrawer(),
+        endDrawer: const MyProfileDrawer(),
+        appBar: const MyAppBar(),
+        body: Column(children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              GridView.builder(
-                  clipBehavior: Clip.hardEdge,
-                  physics: const ScrollPhysics(),
-                  shrinkWrap: true,
-                  scrollDirection: Axis.vertical,
-                  itemCount: boxMovies.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4, childAspectRatio: 10 / 20),
-                  itemBuilder: (context, index) {
-                    watchedmovie movie = boxMovies.getAt(index);
-                    return InkWell(
-                      borderRadius: BorderRadius.circular(12),
-                      splashColor: Theme.of(context).colorScheme.primary,
-                      onTap: () {
-                        Future.delayed(const Duration(milliseconds: 300), () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => movieDescriptionView(
-                                      movieID: movie.movieID)));
-                        });
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12),
-                            color: Colors.grey),
-                        margin: const EdgeInsets.all(5),
-                        padding: const EdgeInsets.all(5),
-                        width: 140,
-                        child: Column(
-                          children: [
-                            Stack(
-                              children: [
-                                Container(
-                                  height: 125,
-                                  decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        image: NetworkImage(
-                                            'https://image.tmdb.org/t/p/w500${movie.posterurl}'),
-                                        fit: BoxFit.fill),
-                                    borderRadius: BorderRadius.circular(12),
-                                  ),
-                                ),
-                                Positioned(
-                                    top: -8,
-                                    left: -8,
-                                    height: 65,
-                                    width: 65,
-                                    child: IconButton(
-                                      splashColor: Colors.purple,
-
-                                      //popup that asks if movie should be deleted from list
-                                      onPressed: isShown == true
-                                          ? () => _deleteMovie(
-                                              context, movie, index)
-                                          : null,
-
-                                      icon: Icon(
-                                        Icons.remove_circle,
-                                        size: 30,
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .primary,
-                                      ),
-                                      alignment: Alignment.topLeft,
-                                    )),
-                              ],
-                            ),
-                            RatingBar(
-                                ignoreGestures: true,
-                                itemPadding:
-                                    const EdgeInsets.only(top: 2, bottom: 2),
-                                itemSize: 15,
-                                itemCount: 5,
-                                allowHalfRating: true,
-                                initialRating: movie.starRating,
-                                ratingWidget: RatingWidget(
-                                    full: const Icon(
-                                      Icons.star,
-                                      color: Colors.black,
-                                    ),
-                                    half: const Icon(
-                                      Icons.star_half,
-                                      color: Colors.black,
-                                    ),
-                                    empty: const Icon(
-                                      Icons.star_outline,
-                                      color: Colors.black,
-                                    )),
-                                onRatingUpdate: (value) {}),
-                            Expanded(
-                              child: Center(
-                                child: Text(
-                                  movie.movieTitle,
-                                  overflow: TextOverflow.fade,
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                      fontSize: 8, fontWeight: FontWeight.w700),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  }),
+              const SizedBox(height: 50, width: 50),
+              const SizedBox(height: 50, width: 50),
+              Container(
+                padding: const EdgeInsets.only(
+                    top: 5, bottom: 5, left: 15, right: 15),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.grey),
+                child: const Text(
+                  "Your Watched Movies",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                width: 40,
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.search,
+                    color: Colors.deepPurple,
+                    size: 25,
+                  ),
+                  onPressed: () {
+                    //Search Movie from list
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                width: 40,
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.view_list,
+                    color: Colors.deepPurple,
+                    size: 25,
+                  ),
+                  onPressed: () {
+                    //Filter list by different criteria
+                  },
+                ),
+              ),
             ],
           ),
-        ),
-      ]),
+          const Padding(padding: EdgeInsets.only(top: 10)),
+          SizedBox(
+            height: MediaQuery.of(context).size.height - 150,
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                GridView.builder(
+                    clipBehavior: Clip.hardEdge,
+                    physics: const ScrollPhysics(),
+                    shrinkWrap: true,
+                    scrollDirection: Axis.vertical,
+                    itemCount: boxMovies.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4, childAspectRatio: 10 / 20),
+                    itemBuilder: (context, index) {
+                      watchedmovie movie = boxMovies.getAt(index);
+                      return InkWell(
+                        borderRadius: BorderRadius.circular(12),
+                        splashColor: Theme.of(context).colorScheme.primary,
+                        onTap: () {
+                          Future.delayed(const Duration(milliseconds: 300), () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => movieDescriptionView(
+                                        movieID: movie.movieID)));
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.grey),
+                          margin: const EdgeInsets.all(5),
+                          padding: const EdgeInsets.all(5),
+                          width: 140,
+                          child: Column(
+                            children: [
+                              Stack(
+                                children: [
+                                  Container(
+                                    height: 125,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: NetworkImage(
+                                              'https://image.tmdb.org/t/p/w500${movie.posterurl}'),
+                                          fit: BoxFit.fill),
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                  ),
+                                  Positioned(
+                                      top: -8,
+                                      left: -8,
+                                      height: 65,
+                                      width: 65,
+                                      child: IconButton(
+                                        splashColor: Colors.purple,
+
+                                        //popup that asks if movie should be deleted from list
+                                        onPressed: isShown == true
+                                            ? () => _deleteMovie(
+                                                context, movie, index)
+                                            : null,
+
+                                        icon: Icon(
+                                          Icons.remove_circle,
+                                          size: 30,
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .primary,
+                                        ),
+                                        alignment: Alignment.topLeft,
+                                      )),
+                                ],
+                              ),
+                              RatingBar(
+                                  ignoreGestures: true,
+                                  itemPadding:
+                                      const EdgeInsets.only(top: 2, bottom: 2),
+                                  itemSize: 15,
+                                  itemCount: 5,
+                                  allowHalfRating: true,
+                                  initialRating: movie.starRating,
+                                  ratingWidget: RatingWidget(
+                                      full: const Icon(
+                                        Icons.star,
+                                        color: Colors.black,
+                                      ),
+                                      half: const Icon(
+                                        Icons.star_half,
+                                        color: Colors.black,
+                                      ),
+                                      empty: const Icon(
+                                        Icons.star_outline,
+                                        color: Colors.black,
+                                      )),
+                                  onRatingUpdate: (value) {}),
+                              Expanded(
+                                child: Center(
+                                  child: Text(
+                                    movie.movieTitle,
+                                    overflow: TextOverflow.fade,
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                        fontSize: 8,
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+              ],
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
